236f2e3ad800f07a87d30437030c5a6ee1a3a165
Ritik dangi <ritikdangi445566@gmail.com>
2025-09-22 23:56:37 +0530

    java basic  part 1

---
+++ src/com/engineering/JavaBasic/Test.java
@@ -0,0 +1,10 @@
+package com.engineering.JavaBasic;
+
+public class Test {
+    public static void main(String[] args) {
+   float a= 1.225f;
+   long k = 55747l;
+   
+        System.out.println("hello world");
+    }
+}
